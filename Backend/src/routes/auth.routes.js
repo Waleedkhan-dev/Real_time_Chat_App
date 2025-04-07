e5d615f3e2c67072a6d8fde5bc3d648edbc17ecf
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-04-07 23:36:08 +0500

    jwt authentication

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from "express"
+import { authMiddleware } from "../midleware/auth.midleware.js";
 import { authcheck, logout, signin, signUp, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../Modles/protectRoute.midleware.js";
 const router = express.Router()
-router.post("/signup", signUp)
-router.post("/signin", signin)
+router.post("/signup", authMiddleware, signUp)
+router.post("/signin", authMiddleware, signin)
 router.post("/logout", logout)
 router.post("/update-profile", protectRoute, updateProfile)
 router.get("/check", protectRoute, authcheck)
