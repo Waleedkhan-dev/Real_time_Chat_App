3bd9053df39291a4e84651b77a061393312c8a39
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-04-18 22:33:46 +0500

    auth

--- Backend/src/midleware/auth.midleware.js
+++ Backend/src/midleware/auth.midleware.js
@@ -10,7 +10,7 @@ const authMiddleware = (req, res, next) => {
 
  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
   if (err) {
-   return res.status(403).json({ error: "Forbidden - Invalid token" });
+   return res.status(403).json({ error: "Forbidden - Invalid token please try again" });
   }
   req.user = decoded;
   next();
