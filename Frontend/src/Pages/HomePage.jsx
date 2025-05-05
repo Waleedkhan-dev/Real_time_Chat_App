76b80ad55e87b95b39edfcab9d6a2c2f07f8f8e1
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-05-05 23:10:29 +0500

    chat

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React, { Fragment } from "react"
 const HomePage = () => {
  return (
   <Fragment>
-   <h1>
-    chat
-   </h1>
-   <p>Message</p>
-   <p>unreaad Message</p>
-   <p>status</p>
+   <div className="flex justify-evenly items-center">
+    <h1>
+     chat
+    </h1>
+    <p>Message</p>
+    <p>unreaad Message</p>
+    <p>status</p>
+   </div>
   </Fragment>
  )
 }
