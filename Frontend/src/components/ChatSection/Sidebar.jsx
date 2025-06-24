d66f138430786b1fbf27dc824a11344debc86904
Waleed khan <151666902+waleedkhan69@users.noreply.github.com>
2025-06-24 16:15:13 +0500

    search section

--- Frontend/src/components/ChatSection/Sidebar.jsx
+++ Frontend/src/components/ChatSection/Sidebar.jsx
@@ -19,33 +19,40 @@ const Sidebar = () => {
   ]
   return (
     <Fragment>
-      <div className="flex bg-gray-300 w-[15%] h-[100vh] p-2">
+      <div>
+        <div className="flex bg-gray-300 w-[15%] h-[100vh] p-2">
 
-        <div className=" flex flex-col  justify-center gap-80 h-full items-center">
-          <div className="flex flex-col h-[15vh] items-center   justify-start  gap-3">
-            {sidebarMessagwSectoin.map((value, index) => {
-              return <div key={index}>
-                <p>{value}</p>
-              </div>
-            })}
-          </div>
-          <div className="flex flex-col h-[15vh] items-center   justify-start  gap-3">
-            {sidbarSettingElement.map((value, index) => {
-              return <div key={index}>
-                <p>{value}</p>
-              </div>
-            })}
-          </div>
-        </div>
-        <div className="w-1/2 border-r bg-gray-300 border-gray-300" >
-
-          {contact.map((val, index) => {
-            <div key={index}>
-              <p className="text-xl font-medium">{val}</p>
-              <p className="text-xl font-medium">Last message....</p>
+          <div className=" flex flex-col  justify-center gap-80 h-full items-center">
+            <div className="flex flex-col h-[15vh] items-center   justify-start  gap-3">
+              {sidebarMessagwSectoin.map((value, index) => {
+                return <div key={index}>
+                  <p>{value}</p>
+                </div>
+              })}
             </div>
-          })}
+            <div className="flex flex-col h-[15vh] items-center   justify-start  gap-3">
+              {sidbarSettingElement.map((value, index) => {
+                return <div key={index}>
+                  <p>{value}</p>
+                </div>
+              })}
+            </div>
+          </div>
+          <div className="w-1/2 border-r bg-gray-300 border-gray-300" >
+
+            {contact.map((val, index) => {
+              <div key={index}>
+                <p className="text-xl font-medium">{val}</p>
+                <p className="text-xl font-medium">Last message....</p>
+              </div>
+            })}
+          </div>
+          <div>
+            <p>search</p>
+            <input type="text" />
+          </div>
         </div>
+
       </div>
     </Fragment>
   )
